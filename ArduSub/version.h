10472b08b7492e9b92c74a8055277a1181a2477e
Jacob Walser <jwalser90@gmail.com>
2016-12-14 17:54:29 -0500

    Sub: Stable release candidate v3.4-rc3
    Bugfix for gcs failsafe

--- ArduSub/version.h
+++ ArduSub/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduSub V3.4-RC2"
+#define THISFIRMWARE "ArduSub V3.4-RC3"
 #define FIRMWARE_VERSION 3,4,0,FIRMWARE_VERSION_TYPE_RC
 
 #ifndef GIT_VERSION
